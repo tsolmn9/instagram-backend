84aefda61b3de6dc48aaacfa1dc7c0a77f187126
24HP0016 24HP0016 <24HP0016@admins-iMac.local>
2024-12-17 17:50:59 +0800

    bug fixed

--- routes/likesRoute.js
+++ routes/likesRoute.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const { likedPost, disLike, getLikes } = require("../controllers/likesCtrl");
+const authMiddleware = require("../authMiddleware");
 const likeRouter = Router();
 
-likeRouter.post("/likedPost", likedPost);
+likeRouter.post("/likedPost", authMiddleware, likedPost);
 likeRouter.delete("/disLike", disLike);
 
 module.exports = likeRouter;
